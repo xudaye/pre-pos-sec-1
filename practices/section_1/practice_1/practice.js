9930566b5d5cf29c76e6949cd1a0ce8a6769adc2
xubingxu <xubingxu110@163.com>
2016-06-01 22:21:36 +0800

    pre

---
+++ practices/section_1/practice_1/practice.js
@@ -0,0 +1,10 @@
+function collect_same_elements(collection_a, collection_b) {
+	var arr = new Array();
+	for(var i = 0;i < collection_a.length;i++ ){
+		if(collection_b.indexOf(collection_a[i]) != -1)
+			arr.push(collection_a[i]);
+	}
+	return arr;
+}
+
+module.exports = collect_same_elements;
